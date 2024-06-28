4b073a4ff3521c4162864b699ec61673de07d7fc
Ranjeet R Patil <67732491+itzranjeet@users.noreply.github.com>
2024-06-28 12:58:08 +0900

    Update yuv_reader.cpp

--- src/yuv_reader.cpp
+++ src/yuv_reader.cpp
@@ -1,5 +1,4 @@
-#include </home/kpit/ranjeetp1/YUV_422_Reader/include/yuv_reader.hpp>
-
+#include </home/smartap/yuv422_reader-main/include/yuv_reader.hpp>
 void YUVREADER::readYUV()
 {
     std::ifstream is(in_yuv_file, std::ifstream::binary);
@@ -8,11 +7,13 @@ void YUVREADER::readYUV()
         is.seekg(0, is.end);
         int length = is.tellg();
         is.seekg(0, is.beg);
+
         char *buffer = new char[length];
+        char *rgbaData_ = new char[length * 2];
         std::cout << "Reading " << length << " characters... ";
         is.read(buffer, length);
         if (is)
-            std::cout << "all characters read successfully.";
+            std::cout << "all characters read successfully.\n";
         else
             std::cout << "error: only " << is.gcount() << " could be read";
         is.close();
@@ -22,16 +23,23 @@ void YUVREADER::readYUV()
         // Four 8-bit unsigned components (CCIR 601) are packed into one 32-bit little-endian word.
         //         Word
         // Decreasing Address Order
-        // Byte 3      Byte 2     Byte 1		Byte 0
-        // Y1		Cr 0		YO	Cb 0
+        // Byte 3      Byte 2     Byte 1                Byte 0
+        // Y1           Cr 0            YO      Cb 0
         // 76543210 76543210 76543210 76543210
         // int framesize = (Width 16/8) Height rowbytes Height
         // In this format, two pixels fit into 32 bits or 4 bytes, so one pixel fits into 16 bits or 2 bytes.
         // For the row bytes calculation, the image width is multiplied by the number of bytes per pixel.
         //  For the frame size calculation, the row bytes are simply multiplied by the number of rows in the frame.
-        writeCSV(length, buffer, csv_filename);
+        unsigned char *uyvyData = (unsigned char *)(buffer);
+        unsigned char *rgbaData = (unsigned char *)(rgbaData_);
+
+        uyvyToRGBA8888(uyvyData, rgbaData, 176, 144);
+        // writeCSV(length, buffer, csv_filename);
         delete[] buffer;
+        delete[] rgbaData_;
     }
+    else
+        std::cout << "File is not available" << std::endl;
 }
 
 void YUVREADER::writeCSV(int len_, char *buf_, std::string csv_filename)
@@ -88,10 +96,48 @@ void YUVREADER::writeYUV()
     std::cout << count << " all characters read successfully." << std::endl;
 }
 
+void YUVREADER::uyvyToRGBA8888(unsigned char *uyvyData, unsigned char *rgbaData, int width, int height)
+{
+    int rgba_index = 0;
+    for (int i = 0; i < width * height * 4 * 3; i += 4)
+    {
+        unsigned char u = uyvyData[i + 0];
+        unsigned char y1 = uyvyData[i + 1];
+        unsigned char v = uyvyData[i + 2];
+        unsigned char y2 = uyvyData[i + 3];
+
+        int c = y1 - 16;
+        int d = u - 128;
+        int e = v - 128;
+
+        rgbaData[rgba_index++] = std::max(0, std::min(255, (298 * c + 409 * e + 128) >> 8));           // R
+        rgbaData[rgba_index++] = std::max(0, std::min(255, (298 * c - 100 * d - 208 * e + 128) >> 8)); // G
+        rgbaData[rgba_index++] = std::max(0, std::min(255, (298 * c + 516 * d + 128) >> 8));           // B
+        rgbaData[rgba_index++] = 255;                                                                  // A
+        c = y2 - 16;
+
+        rgbaData[rgba_index++] = std::max(0, std::min(255, (298 * c + 409 * e + 128) >> 8));           // R
+        rgbaData[rgba_index++] = std::max(0, std::min(255, (298 * c - 100 * d - 208 * e + 128) >> 8)); // G
+        rgbaData[rgba_index++] = std::max(0, std::min(255, (298 * c + 516 * d + 128) >> 8));           // B
+        rgbaData[rgba_index++] = 255;                                                                  // A
+    }
+    std::cout << rgba_index << std::endl;
+
+    // cv::Mat save_img;
+    // rgbaData >> save_img;
+
+    // if (save_img.empty())
+    // {
+    //     std::cerr << "Something is wrong with the webcam, could not get frame." << std::endl;
+    // }
+    // cv::imwrite("test.png", save_img);
+}
+
+
 int main()
 {
     YUVREADER obj;
-    // obj.readYUV();
-    obj.writeYUV();
+    obj.readYUV();
+    // obj.writeYUV();
     return 0;
 }
