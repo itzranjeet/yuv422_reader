974c210e446c875326e8877f047e9a160332cfba
Ranjeet R Patil <67732491+itzranjeet@users.noreply.github.com>
2024-06-28 12:59:36 +0900

    Update yuv_reader.hpp

--- include/yuv_reader.hpp
+++ include/yuv_reader.hpp
@@ -2,16 +2,18 @@
 #include <fstream>
 #include <string>
 #include <sstream>
+// #include "./../../../../../usr/include/opencv4/opencv2/opencv.hpp"
 
 class YUVREADER
 {
 public:
     // PATHS
-    std::string in_yuv_file = "/home/kpit/ranjeetp1/YUV_422_Reader/1.yuv";
-    std::string csv_filename = "/home/kpit/ranjeetp1/YUV_422_Reader/1.csv";
-    std::string op_yuv_file = "/home/kpit/ranjeetp1/YUV_422_Reader/2.yuv";
+    std::string in_yuv_file = "/home/smartap/yuv422_reader-main/input/1.yuv";
+    std::string csv_filename = "/home/smartap/yuv422_reader-main/input/1.csv";
+    std::string op_yuv_file = "/home/smartap/yuv422_reader-main/input/2.yuv";
 
     void readYUV();
     void writeCSV(int, char *, std::string);
     void writeYUV();
+    void uyvyToRGBA8888(unsigned char *, unsigned char *, int, int);
 };
